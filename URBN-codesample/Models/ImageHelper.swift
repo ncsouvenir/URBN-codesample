dfc78037abd4fa43650af02c3fc40827425bcd4b
Nicole Souvenir <github@nicolesouvenir.com>
2018-04-12 13:31:49 -0400

    added image and network helpers

--- URBN-codesample/Models/ImageHelper.swift
+++ URBN-codesample/Models/ImageHelper.swift
@@ -8,5 +8,32 @@
 
 import UIKit
 
+enum ImageError: Error {
+    case badUrl
+    case badData
+}
 
+class ImageHelper {
+    private init() {}
+    static let manager = ImageHelper()
+    func getImage(from urlStr: String,
+                  completionHandler: @escaping (UIImage) -> Void,
+                  errorHandler: @escaping (Error) -> Void) {
+        guard let url = URL(string: urlStr) else {
+            errorHandler(ImageError.badUrl)
+            return
+        }
+        let completion = {(data: Data) in
+            if let onlineImage = UIImage(data: data) {
+                completionHandler(onlineImage)
+            } else {
+                errorHandler(ImageError.badData)
+            }
+        }
+        let request = URLRequest(url: url)
+        NetworkHelper.manager.performDataTask(with: request,
+                                              completionHandler: completion,
+                                              errorHandler: errorHandler)
+    }
+}
 
